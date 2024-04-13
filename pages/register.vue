ab17857b1580a770ae11bc7f82df38d27e512969
tfulanchan <60479975+tfulanchan@users.noreply.github.com>
2024-04-13 22:33:26 +0300

    Initial commit

---
+++ pages/register.vue
@@ -0,0 +1,62 @@
+<script setup lang="ts">
+import axios from "axios";
+
+definePageMeta({
+  layout: "centered",
+});
+
+interface RegisterPayload {
+  name: String;
+  email: String;
+  password: String;
+  password_confirmation: String;
+}
+
+const form = ref({
+  name: "",
+  email: "",
+  password: "",
+  password_confirmation: "",
+});
+
+async function register(payload: RegisterPayload) {
+  const res = await axios.post("http://localhost/api/register", payload);
+  console.log(res);
+}
+</script>
+<template>
+  <div class="register">
+    <h1>Register</h1>
+    <!-- <pre>
+      {{ form }}
+    </pre> -->
+    <form @submit.prevent="register(form)">
+      <label>
+        <div>Name</div>
+        <input type="text" v-model="form.name" />
+      </label>
+
+      <label>
+        <div>Email</div>
+        <input type="email" v-model="form.email" />
+      </label>
+
+      <label>
+        <div>Password</div>
+        <input type="password" v-model="form.password" />
+      </label>
+
+      <label>
+        <div>Confirm Password</div>
+        <input type="password" v-model="form.password_confirmation" />
+      </label>
+
+      <button class="btn">Register</button>
+    </form>
+
+    <p>
+      Already have an account?
+      <NuxtLink class="underline text-lime-600" to="/login">Login</NuxtLink>
+    </p>
+  </div>
+</template>
