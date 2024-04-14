aab99067769756cc6c437c26d60411d274f26203
tfulanchan <60479975+tfulanchan@users.noreply.github.com>
2024-04-14 20:07:33 +0300

    Update the Guest Middleware to use the Cached User
    redirect /login to /me if user is logged in

--- middleware/guest.ts
+++ middleware/guest.ts
@@ -1,10 +1,15 @@
 import axios, { AxiosError } from "axios";
 
 export default defineNuxtRouteMiddleware(async (to, from) => {
-  try {
-    const { data: user} = await axios.get("/user");
-    if (user){
-        return navigateTo("/me");
-    }
-  } catch (error) {}
+  const { user, initUser } = useAuth();
+  await initUser();
+  if (user.value) {
+    return navigateTo("/me");
+  }
+  // try {
+  //   const { data: user } = await axios.get("/user");
+  //   if (user) {
+  //     return navigateTo("/me");
+  //   }
+  // } catch (error) {}
 });
