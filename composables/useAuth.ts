cce72f234b01981793cd81edf11a7bfaa1f8bfd4
tfulanchan <60479975+tfulanchan@users.noreply.github.com>
2024-04-14 19:43:23 +0300

    Format the User Data Once
    created a getUser function within the useAuth composable to fetch the user data and format it with proper Date data types where applicable

--- composables/useAuth.ts
+++ composables/useAuth.ts
@@ -1,6 +1,36 @@
 // composable
 import axios from "axios";
 export const useAuth = () => {
+  interface User {
+    email: string;
+    email_verified_at?: Date;
+    id: number;
+    name: string;
+    two_factor_confirmed_at?: Date;
+    two_factor_recovery_codes?: number;
+    two_factor_secret?: string;
+    updated_at: Date;
+    created_at: Date;
+  }
+  async function getUser(): Promise<User | null> {
+    try {
+      const res = await axios.get("/user");
+      const user = res.data;
+      return {
+        ...user,
+        created_at: new Date(user.created_at),
+        updated_at: new Date(user.updated_at),
+        email_verified_at: user.email_verified_at
+          ? new Date(user.email_verified_at)
+          : null,
+        two_factor_confirmed_at: user.two_factor_confirmed_at
+          ? new Date(user.two_factor_confirmed_at)
+          : null,
+      };
+    } catch (err) {
+      return null;
+    }
+  }
   interface LoginPayload {
     email: string;
     password: string;
